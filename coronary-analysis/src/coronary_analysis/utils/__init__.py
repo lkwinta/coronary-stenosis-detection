eaab40e255e60e63c7ab4223f29fa2f7b85421f7
Kamil Rudny <k.rudny1@gmail.com>
2026-05-16 18:51:03 +0200

    Move device functions to utils

--- coronary-analysis/src/coronary_analysis/utils/__init__.py
+++ coronary-analysis/src/coronary_analysis/utils/__init__.py
@@ -1,4 +1,5 @@
 from .visualization import show_image, plot_lines
-from .device import get_device
+from .device import get_device, clear_device_cache
+from .seed import set_seed
 
-__all__ = ["show_image", "plot_lines", "get_device"]
+__all__ = ["show_image", "plot_lines", "get_device", "clear_device_cache", "set_seed"]
---
+++ coronary-analysis/src/coronary_analysis/utils/seed.py
@@ -0,0 +1,19 @@
+import random
+
+import numpy as np
+import torch
+
+from .device import get_device
+
+
+def set_seed(seed: int) -> None:
+    device = get_device()
+
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    if device.type == "cuda":
+        torch.cuda.manual_seed_all(seed)
+    elif device.type == "mps":
+        torch.mps.manual_seed(seed)
